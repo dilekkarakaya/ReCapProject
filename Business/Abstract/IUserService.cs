2ff79cb24673e008ac3250ae222f9a6e813153f4
dilekkarakaya <dilekkrky34@gmail.com>
2023-01-10 15:53:29 +0300

    10/02/2023

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,8 @@ namespace Business.Abstract
 {
     public interface IUserService
     {
+        IResult Add(User user);
+        IResult Delete(User user);
+        
     }
 }
