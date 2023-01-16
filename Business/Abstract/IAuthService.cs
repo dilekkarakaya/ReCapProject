a8960000dfce6846e42d034a958083c44f8d3bd1
dilekkarakaya <dilekkrky34@gmail.com>
2023-01-16 16:00:02 +0300

    .

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Security.JWT;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password); //Kayıt operasyonu
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);//Giriş operasyonu
+        IResult UserExists(string email);//Kullanıcı var mı
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,83 @@
+﻿using Business.Abstract;
+using Core.Security.Hashing;
+using Core.Security.JWT;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+   public class AuthManager:IAuthService
+    {
+        IUserService _userService;
+        ITokenHelper _tokenHelper;
+        public AuthManager(IUserService userService,ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            var user = new User
+            {
+                Email = userForRegisterDto.Email,
+                FirstName = userForRegisterDto.FirstName,
+                LastName = userForRegisterDto.LastName,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Status = true
+            };
+            _userService.Add(user);
+            return new SuccessDataResult<User>(user, "kayıt olundu");
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            var userToCheck = _userService.GetByMail(userForLoginDto.Email);
+            if (userToCheck.Data == null)
+            {
+                return new ErrorDataResult<User>("kulklanıcı bulunamadı");
+            }
+
+            if (!HashingHelper.VerifyPasswordHash(userForLoginDto.Password, userToCheck.Data.PasswordHash, userToCheck.Data.PasswordSalt))
+            {
+                return new ErrorDataResult<User>("yanlkış şifre");
+            }
+
+            return new SuccessDataResult<User>(userToCheck.Data, "başarılı giriş");
+        }
+
+        public IResult UserExists(string email)
+        {
+            IResult result = BusinessRules.Run(CheckIfUserExist(email));
+            if (result != null)
+            {
+                return new ErrorResult("kullanıcı mevcut");
+            }
+            return new SuccesResult();
+        }
+
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            var claims = _userService.GetClaims(user);
+            var accessToken = _tokenHelper.CreateToken(user, claims.Data);
+            return new SuccessDataResult<AccessToken>(accessToken, "token oluşturuldu");
+        }
+        private IResult CheckIfUserExist(string email)
+        {
+            var result = _userService.GetByMail(email).Data;
+            if (result != null)
+            {
+                return new ErrorResult();
+            }
+            return new SuccesResult();
+        }
+    }
+}
