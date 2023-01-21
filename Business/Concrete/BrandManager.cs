b28097856281f2dce4432703a3a7230f586a6ec8
dilekkarakaya <dilekkrky34@gmail.com>
2023-01-21 22:16:18 +0300

    .

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,11 +1,13 @@
 ﻿using Business.Abstract;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -17,30 +19,69 @@ namespace Business.Concrete
         {
             _brandDal = brandDal;
         }
+
         [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
-            
+            IResult result = BusinessRules.Run(CheckIfBrandNameExists(brand.BrandName));
+            if (result != null)
+            {
+                return new ErrorResult();
+            }
+            _brandDal.Add(brand);
+            return new SuccesResult();
+        }
+
+        private IResult CheckIfBrandNameExists(string brandName)
+        {
+            var result = _brandDal.GetAll(b => b.BrandName == brandName).Any();
+            if (result)
+            {
+                return new ErrorResult();
+            }
+            return new SuccesResult();
         }
 
         public IResult Delete(Brand brand)
         {
-            throw new NotImplementedException();
+            IResult result = BusinessRules.Run(CheckIfBrandExists(brand.BrandId));
+            if (result != null)
+            {
+                return new ErrorResult();
+            }
+            _brandDal.Delete(brand);
+            return new SuccesResult();
+        }
+
+        private IResult CheckIfBrandExists(int brandId)
+        {
+            var result = _brandDal.GetAll(b => b.BrandId == brandId).Any();
+            if (!result)
+            {
+                return new ErrorResult();
+            }
+            return new SuccesResult();
         }
 
         public IDataResult<List<Brand>> GetAll()
         {
-            throw new NotImplementedException();
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll());
         }
 
         public IDataResult<Brand> GetAllById(int id)
         {
-            throw new NotImplementedException();
+            return new SuccessDataResult<Brand>(_brandDal.Get(b => b.BrandId == id));
         }
 
         public IResult Update(Brand brand)
         {
-            throw new NotImplementedException();
+            IResult result = BusinessRules.Run(CheckIfBrandNameExists(brand.BrandName));
+            if (result != null)
+            {
+                return new ErrorResult();
+            }
+            _brandDal.Update(brand);
+            return new SuccesResult();
         }
     }
 }
